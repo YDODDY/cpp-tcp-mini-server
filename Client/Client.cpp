e00cadfd49a29744164fce6b868df95993343877
YDODDY <loudlyhigh7@gmail.com>
2026-08-19 23:10:03 +0900

    Initial commit.

---
+++ Client/Client.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "TCP Client\n";
+    return 0;
+}
+
---
+++ Server/Server.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "TCP Server\n";
+    return 0;
+}
+
